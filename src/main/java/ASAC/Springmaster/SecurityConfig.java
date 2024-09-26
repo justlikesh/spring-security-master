490d9a4fccc6fac421789afb1660c5f7edfef67a
justlikesh <lpsac@naver.com>
2024-09-26 18:03:29 +0900

    Feat : formLogin() API 설정
    - yml 에서 user 권한 없애기
    - loginPage, home API 구현
    - 인증을 받지 못했을경우에 인증을 받도록 하는 방식에서 formLogin 설정으로 변경

--- src/main/java/ASAC/Springmaster/SecurityConfig.java
+++ src/main/java/ASAC/Springmaster/SecurityConfig.java
@@ -1,15 +1,24 @@
 package ASAC.Springmaster;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -19,7 +28,22 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {  // 여기서 별도의 설정을 할수잇다
         http                                                     // 인증, 인가 정책을 성정할수잇다
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())      //http 설정에대한 인가설정을 하겠다 어떤 요청에도 인증을 받겠따
-                .formLogin(Customizer.withDefaults()); //인증을 받지 못햇을경우에 인증을 받도록 하는방식  디폴트방식
+                .formLogin(form -> form
+//                        .loginPage("/loginPage")
+                        .loginProcessingUrl("/loginProc")
+                        .defaultSuccessUrl("/", true) //ture 주면 어떤 경우라도 디폴트로가고 false 면 해당 url 에 남아잇는다
+                        .usernameParameter("userId")
+                        .passwordParameter("passwd")
+                        .successHandler((request, response, authentication) -> {     // 위에 설정보다 핸들러가 더 우선순위이다
+                            System.out.println("authentication :" + authentication);
+                            response.sendRedirect("/home");
+                        })
+                        .failureHandler((request, response, exception) -> {
+                            System.out.println("exception:" + exception.getMessage());
+                            response.sendRedirect("/login");
+                        })
+                        .permitAll()
+                );
 
         return http.build();
     }
