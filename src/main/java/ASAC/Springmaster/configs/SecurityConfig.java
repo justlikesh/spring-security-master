299cce9d6c295eb7ced3e80555b1b499ba2f6308
justlikesh <lpsac@naver.com>
2024-09-29 07:08:24 +0900

    Feat : 로그인 페이지 구현
    - /css/**, /images/**, /js/**, /favicon.*, /*/icon-* 경로에 대한 접근을 모두 허용
    - 루트 경로 ("/")에 대한 접근을 모두 허용
    - 그 외 모든 요청은 인증 필요
    - 커스텀 로그인 페이지를 /login으로 설정하고, 해당 페이지에 대한 접근은 인증 없이 가능하도록 설정
    - login.html 구현

--- src/main/java/ASAC/Springmaster/configs/SecurityConfig.java
+++ src/main/java/ASAC/Springmaster/configs/SecurityConfig.java
@@ -1,31 +1,15 @@
-package ASAC.Springmaster;
+package ASAC.Springmaster.configs;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -34,14 +18,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {  // 여기서 별도의 설정을 할수잇다
 
-        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
-        requestCache.setMatchingRequestParameterName("customParam=y");
-
-        http                                                     // 인증, 인가 정책을 성정할수잇다
+        http                                                     // 인증, 인가 정책을 설정할수잇다
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(".logoutSuccess").permitAll()
+                        .requestMatchers("/css/**","/images/**","/js/**","/favicon.*","/*/icon-*").permitAll()
+                        .requestMatchers("/").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults()
+                .formLogin(form -> form.loginPage("/login").permitAll()
                 )
         ;
 
