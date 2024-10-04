d779b4c42e99077e819e083293d214a15c556d9b
justlikesh <lpsac@naver.com>
2024-10-04 16:57:38 +0900

    Feat: 커스텀 RestAuthenticationFilter 및 토큰 클래스 구현
    - POST 요청에 대해 RestAuthenticationFilter 구현
    - JSON 데이터를 이용한 인증 시도 로직 추가
    - 인증 토큰으로 RestAuthenticationToken 클래스 구현
    - Ajax 요청을 확인하는 WebUtil 메서드 추가

--- src/main/java/ASAC/Springmaster/security/configs/SecurityConfig.java
+++ src/main/java/ASAC/Springmaster/security/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ASAC.Springmaster.security.configs;
 
+import ASAC.Springmaster.security.filters.RestAuthenticationFilter;
 import ASAC.Springmaster.security.handler.FormAccessDeniedHandler;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,6 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 @EnableWebSecurity
@@ -55,7 +59,11 @@ public class SecurityConfig {
 
     @Bean
     @Order(1)
-    public SecurityFilterChain restSecurityFilterChain(HttpSecurity http) throws Exception {  // 여기서 별도의 설정을 할수잇다
+    public SecurityFilterChain restSecurityFilterChain(HttpSecurity http, AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {  // 여기서 별도의 설정을 할수잇다
+
+//        AuthenticationManagerBuilder managerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+//        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
 
         http                                                 // 인증, 인가 정책을 설정할수잇다
                 .securityMatcher("/api/login")
@@ -63,9 +71,17 @@ public class SecurityConfig {
                         .requestMatchers("/css/**","/images/**","/js/**","/favicon.*","/*/icon-*").permitAll()
                         .anyRequest().permitAll())
                 .csrf(AbstractHttpConfigurer::disable)
+//                .addFilterBefore(restAuthenticationFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class)
+//                .authenticationManager(authenticationManager)
         ;
 
         return http.build();
+    }
+
+    private RestAuthenticationFilter restAuthenticationFilter(AuthenticationManager authenticationManager){
+        RestAuthenticationFilter restAuthenticationFilter = new RestAuthenticationFilter();
+        restAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        return restAuthenticationFilter;
     }
 
 
---
+++ src/main/java/ASAC/Springmaster/security/token/RestAuthenticationToken.java
@@ -0,0 +1,36 @@
+package ASAC.Springmaster.security.token;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class RestAuthenticationToken extends AbstractAuthenticationToken {
+
+    private final Object principal;
+    private final Object credentials;
+
+    public RestAuthenticationToken(Collection<? extends GrantedAuthority> authorities, Object credentials, Object principal) {
+        super(authorities);
+        this.credentials = credentials;
+        this.principal = principal;
+        setAuthenticated(true);
+    }
+
+    public RestAuthenticationToken(Object principal, Object credentials) {
+        super(null);
+        this.principal = principal;
+        this.credentials = credentials;
+        setAuthenticated(false);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return this.credentials;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return this.principal;
+    }
+}
