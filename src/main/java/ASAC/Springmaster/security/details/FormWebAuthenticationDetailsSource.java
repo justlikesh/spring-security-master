bc5d8764e4930ac321169fe7b038d0f1f48e5497
justlikesh <lpsac@naver.com>
2024-10-08 16:29:07 +0900

    Feat: secret_key 처리를 위한 FormWebAuthenticationDetails 클래스 추가, html 파일 추가
    - HttpServletRequest에서 secret_key 파라미터를 받아 저장하는 FormWebAuthenticationDetails 구현
    - WebAuthenticationDetails를 상속받아 인증 세부 정보를 확장
    - admin header.html, header.html, home.html, homecontent.html 구현

--- src/main/java/ASAC/Springmaster/security/details/FormWebAuthenticationDetailsSource.java
+++ src/main/java/ASAC/Springmaster/security/details/FormWebAuthenticationDetailsSource.java
@@ -7,9 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class FormWebAuthenticationDetailsSource implements AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> {
-
     @Override
     public WebAuthenticationDetails buildDetails(HttpServletRequest request) {
-        return new FormAuthenticationDetails(request);
+        return new FormWebAuthenticationDetails(request);
     }
 }
